4ce69d81426f17c6d6c4f2ec8fb828422c3bc29e
shakishaki <shaki.canchola@yahoo.com>
2018-07-10 22:33:46 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -44,8 +44,3 @@ while steps < 11
 end
 end
 how_many_steps?
-
-def break_dance
-  # Write a solution that uses the same code as how_many_steps?, but breaks the
-  # loop if steps is equal to 6
-end
